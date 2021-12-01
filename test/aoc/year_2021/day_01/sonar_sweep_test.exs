8fac933c93dc01604c62e5e7b3a8fcefe069be6c
Michael Borohovski <michael.borohovski@gmail.com>
2021-12-01 13:18:17 -0800

    Finish day 1 part 2

--- test/aoc/year_2021/day_01/sonar_sweep_test.exs
+++ test/aoc/year_2021/day_01/sonar_sweep_test.exs
@@ -4,22 +4,22 @@ defmodule Aoc.Year2021.Day01.SonarSweepTest do
 
   alias Aoc.Year2021.Day01.SonarSweep
 
+  @example_input """
+  199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  """
+
   describe "part_1/1" do
     test "examples" do
-      input = """
-      199
-      200
-      208
-      210
-      200
-      207
-      240
-      269
-      260
-      263
-      """
-
-      assert input |> SonarSweep.part_1() == 7
+      assert @example_input |> SonarSweep.part_1() == 7
     end
 
     @tag day: 01, year: 2021
@@ -30,11 +30,12 @@ defmodule Aoc.Year2021.Day01.SonarSweepTest do
 
   describe "part_2/1" do
     test "examples" do
+      assert @example_input |> SonarSweep.part_2() == 5
     end
 
     @tag day: 01, year: 2021
     test "input", %{input: input} do
-      assert input |> SonarSweep.part_2() == input
+      assert input |> SonarSweep.part_2() == 1497
     end
   end
 end
