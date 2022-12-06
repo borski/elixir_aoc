d09d9af13617ce15bee0d856bf4f4bdd93171336
Michael Borohovski <michael.borohovski@gmail.com>
2022-12-06 04:07:18 -0800

    finish day 6 2022. that was way easier than day 5.

---
+++ test/aoc/year_2022/day_06/tuning_trouble_test.exs
@@ -0,0 +1,36 @@
+defmodule Aoc.Year2022.Day06.TuningTroubleTest do
+  use Aoc.DayCase
+  doctest Aoc.Year2022.Day06.TuningTrouble, import: true
+
+  alias Aoc.Year2022.Day06.TuningTrouble, as: Day06
+
+  describe "part_1/1" do
+    test "examples" do
+      assert Day06.part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+      assert Day06.part_1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+      assert Day06.part_1("nppdvjthqldpwncqszvftbrmjlhg") == 6
+      assert Day06.part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+      assert Day06.part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+    end
+
+    @tag day: 06, year: 2022
+    test "input", %{input: input} do
+      assert input |> Day06.part_1() == 1356
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+      assert Day06.part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19
+      assert Day06.part_2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
+      assert Day06.part_2("nppdvjthqldpwncqszvftbrmjlhg") == 23
+      assert Day06.part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29
+      assert Day06.part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26
+    end
+
+    @tag day: 06, year: 2022
+    test "input", %{input: input} do
+      assert input |> Day06.part_2() == 2564
+    end
+  end
+end
